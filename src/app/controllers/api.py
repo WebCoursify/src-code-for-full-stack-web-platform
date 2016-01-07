05caadc621563db32a87b56b1f443a959e663f20
Taiyuan Zhang <taiyuanz@andrew.cmu.edu>
2016-01-07 10:29:40 -0800

    u

--- src/app/controllers/api.py
+++ src/app/controllers/api.py
@@ -118,9 +118,9 @@ def register(request):
     3. Create a user through the User class. The user.role should be User.ROLE_AUTHOR
     4. Return data as indicated above
     """
-    email = request.REQUEST.get('email', None)
-    username = request.REQUEST.get('username', None)
-    password = request.REQUEST.get('password', None)
+    email = get_argument(request, 'email', None)
+    username = get_argument(request, 'username', None)
+    password = get_argument(request, 'password', None)
 
     if not email or not username or not password:
         return HttpResponseBadRequest()
@@ -239,19 +239,6 @@ def article_operation(author_required=False):
         return inner
 
     return decorator
-<<<<<<< 78345cfe2bbc2a49914213ba15cd88627d286128
-=======
-<<<<<<< 8850fb21c0627c4139d288db1cf64a7a816d01c8
->>>>>>> finish model-article
-=======
-
->>>>>>> finish follow relation
-=======
->>>>>>> finish model-article
-=======
-
->>>>>>> finish follow relation
->>>>>>> finish follow relation
 
 
 @csrf_exempt
@@ -366,75 +353,6 @@ def get_followers(request):
 @login_required_otherwise_401
 def get_followings(request):
     user_id = get_argument(request, 'user_id')
-    if user_id is None:
-        user_id = request.session.get('user')['id']
-    else:
-        user_id = int(user_id)
-
-    user = User.objects.find_by_id(user_id)
-
-    followings = user.get_followings()
-
-    return HttpResponse(json.dumps([{'id': u.id, 'username': u.username} for u in followings]))
-
-###############
-# Follow APIs #
-###############
-
-@csrf_exempt
-@login_required_otherwise_401
-@allow_methods(['POST'])
-def follow(request):
-    data = request.REQUEST
-    if 'target_user_id' not in data:
-        return HttpResponseBadRequest('target_user_id must be provided')
-
-    target_user = User.objects.find_by_id(int(data['target_user_id']))
-    if target_user is None:
-        return HttpResponseNotFound('target user not found')
-
-    user = User.objects.find_by_id(request.session.get('user')['id'])
-    user.add_following(target_user)
-
-    return HttpResponse(json.dumps({'success': True}))
-
-
-@csrf_exempt
-@login_required_otherwise_401
-@allow_methods(['POST'])
-def unfollow(request):
-    data = request.REQUEST
-    if 'target_user_id' not in data:
-        return HttpResponseBadRequest('target_user_id must be provided')
-
-    target_user = User.objects.find_by_id(int(data['target_user_id']))
-    if target_user is None:
-        return HttpResponseNotFound('target user not found')
-
-    user = User.objects.find_by_id(request.session.get('user')['id'])
-    user.remove_following(target_user)
-
-    return HttpResponse(json.dumps({'success': True}))
-
-@login_required_otherwise_401
-@allow_methods(['GET'])
-def get_followers(request):
-    user_id = request.REQUEST.get('user_id', None)
-    if user_id is None:
-        user_id = request.session.get('user')['id']
-    else:
-        user_id = int(user_id)
-
-    user = User.objects.find_by_id(user_id)
-
-    followers = user.get_followers()
-
-    return HttpResponse(json.dumps([{'id': u.id, 'username': u.username} for u in followers]))
-
-@allow_methods(['GET'])
-@login_required_otherwise_401
-def get_followings(request):
-    user_id = request.REQUEST.get('user_id', None)
     if user_id is None:
         user_id = request.session.get('user')['id']
     else:
